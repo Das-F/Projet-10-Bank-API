1d1926ae295d9d3439f31a2387c22b519dbceeef
Diane Foucher <152055207+Das-F@users.noreply.github.com>
2025-10-29 16:31:59 +0100

    header corrected

--- Front/front/src/components/Header.jsx
+++ Front/front/src/components/Header.jsx
@@ -5,14 +5,16 @@ import icon from "../assets/img/circle-user-solid-full.svg";
 function Header() {
   return (
     <nav className="nav">
-      <div className="nav-logo">
-        <img className="nav-logo-image" src={logo} alt="Argent Bank Logo" />
-      </div>
-      <div>
-        <a className="nav-item" href="./sign-in.html">
-          <img className="nav-user-icon" src={icon} alt="User Icon" />
-          Sign In
-        </a>
+      <div className="nav-container">
+        <div className="nav-logo">
+          <img className="nav-logo-image" src={logo} alt="Argent Bank Logo" />
+        </div>
+        <div>
+          <a className="nav-item" href="./sign-in.html">
+            <img className="nav-user-icon" src={icon} alt="User Icon" />
+            Sign In
+          </a>
+        </div>
       </div>
     </nav>
   );
