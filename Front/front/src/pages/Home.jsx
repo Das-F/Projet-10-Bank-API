b5953cf6d32ae15fe57e144930329d60d280aa39
Diane Foucher <152055207+Das-F@users.noreply.github.com>
2025-10-30 14:05:34 +0100

    homepage layout corrected

--- Front/front/src/pages/Home.jsx
+++ Front/front/src/pages/Home.jsx
@@ -1,6 +1,9 @@
-import "../styles/home.css";
+import "../styles/Home.css";
 import Header from "../components/Header";
 import backgroundImage from "../assets/img/bank-tree.jpeg";
+import chatIcon from "../assets/img/icon-chat.png";
+import moneyIcon from "../assets/img/icon-money.png";
+import securityIcon from "../assets/img/icon-security.png";
 import Footer from "../components/Footer";
 
 function Home() {
@@ -8,15 +11,31 @@ function Home() {
     <>
       <Header />
       <div className="hero">
-        <img src={backgroundImage} className="Banking-background" alt="Banking background" />
+        <img src={backgroundImage} className="banking-background" alt="Banking background" />
         <section className="hero-content">
-          <h2 className="sr-only">Promoted Content</h2>
           <p className="subtitle">No fees.</p>
           <p className="subtitle">No minimum deposit.</p>
           <p className="subtitle">High interest rates.</p>
           <p className="text">Open a savings account with Argent Bank today!</p>
         </section>
       </div>
+      <section class="features">
+        <div className="feature-item">
+          <img src={chatIcon} alt="Chat Icon" className="feature-icon" />
+          <h3 className="feature-item-title">You are our #1 priority</h3>
+          <p>Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.</p>
+        </div>
+        <div className="feature-item">
+          <img src={moneyIcon} alt="Money Icon" className="feature-icon" />
+          <h3 className="feature-item-title">More savings means higher rates</h3>
+          <p>The more you save with us, the higher your interest rate will be!</p>
+        </div>
+        <div className="feature-item">
+          <img src={securityIcon} alt="Security Icon" className="feature-icon" />
+          <h3 className="feature-item-title">Security you can trust</h3>
+          <p>We use top of the line encryption to make sure your data and money is always safe.</p>
+        </div>
+      </section>
       <Footer />
     </>
   );
