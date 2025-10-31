685f46a4b44441556f2908af536e588841d52a0a
Diane Foucher <152055207+Das-F@users.noreply.github.com>
2025-10-31 12:45:58 +0100

    User page added

--- Front/front/src/pages/User.jsx
+++ Front/front/src/pages/User.jsx
@@ -1,18 +1,37 @@
 import "../styles/User.css";
 import { Link } from "react-router-dom";
-import icon from "../assets/img/circle-user-solid-full.svg";
 import HeaderSignOut from "../components/Header-sign-out";
 import Footer from "../components/Footer";
+
+const username = "User";
 
 function User() {
   return (
     <>
       <HeaderSignOut />
-      <div className="user-page">
-        <h1>User Page</h1>
-        <img src={icon} alt="User Icon" />
-        <p>Welcome to your user page!</p>
-        <Link to="/">Go back to Home</Link>
+      <div className="background">
+        <div className="user-page">
+          <h1>
+            Welcome back
+            <br />
+            {username} !
+          </h1>
+        </div>
+        <div className="edit-name-container">
+          <a className="edit-button" href="./edit-name.html">
+            Edit Name
+          </a>
+        </div>
+        <section className="account">
+          <div className="account-content-wrapper">
+            <h3 className="account-title">Argent Bank Checking (x8349)</h3>
+            <p className="account-amount">$2,082.79</p>
+            <h3 className="account-amount-description">Available Balance</h3>
+          </div>
+          <div className="account-content-wrapper-cta">
+            <button className="transaction-button">View transactions</button>
+          </div>
+        </section>
       </div>
       <Footer />
     </>
